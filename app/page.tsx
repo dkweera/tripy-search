0efab413bbee3c2d1e595286ff07cb2462096fa5
dhruv.kweera <kweeradhruv08@gmail.com>
2026-01-29 23:07:27 +0530

    search bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { products } from "../data/products";
 import SearchClient from "./search-client";
 
-export const dynamic = "force-static"; // SSG
+export const dynamic = "force-static";
 
 export default function Page() {
   return <SearchClient products={products} />;
